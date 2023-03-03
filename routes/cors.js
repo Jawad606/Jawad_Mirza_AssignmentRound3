e18c062765df298e8b0f00dabde14dc23362449f
darksoul <jawadmirza606@gmail.com>
2023-03-04 00:31:48 +0500

    first commit

---
+++ routes/cors.js
@@ -0,0 +1,25 @@
+// this is main the file which authenticate request
+// only request from these lnks are authenticated.
+const express = require("express");
+const cors = require("cors");
+// these links are used to for fontend. Only these link request are authenticated
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:5000",
+  'https://dm-testing-be-dev-trial.herokuapp.com/',
+  'https://jawad606.github.io'
+
+];
+var corsOptionsDelegate = (req, callback) => {
+  var corsOptions;
+  console.log(req.header("Origin"));
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
+exports.cors = cors();
+exports.corsWithOptions = cors(corsOptionsDelegate);
